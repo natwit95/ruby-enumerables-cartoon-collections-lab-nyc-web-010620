07cdee401fdb63c3939186f8d40b9800fa5bf14e
Natalia  Wit <natwit95@gmail.com>
2020-01-03 05:55:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,9 @@
 def roll_call_dwarves(array)# code an argument here
   # Your code here
-  array.each_with_index do |dwarf,index|
-puts "#{index + 1}. #{dwarf}"
+  array.each_with_index do |array,index|
+puts "#{index + 1}. #{array}"
 end
-end 
+end
 
 roll_call_dwarves(array)
 def summon_captain_planet# code an argument here
