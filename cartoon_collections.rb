3a1d84a6ab63712300f806571a758beb1d868206
Natalia  Wit <natwit95@gmail.com>
2020-01-03 05:58:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(array)# code an argument here
   # Your code here
-  array.each_with_index do |array, index|
-puts "#{index + 1}. #{array}"
+  array.each_with_index do |item, index|
+puts "#{index + 1}. #{item}"
 end
 end
 
