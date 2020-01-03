0b6d04f0713ab7eccdc2757d5360d33eb5446046
Natalia  Wit <natwit95@gmail.com>
2020-01-03 05:52:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,6 @@
 def roll_call_dwarves(array)# code an argument here
   # Your code here
- puts array.each do
-end
+puts "#{index + 1}. #{array}"
 end
 
 def summon_captain_planet# code an argument here
